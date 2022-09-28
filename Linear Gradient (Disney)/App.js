6abf8b73738b206404129d49d5210350977e4e6e
Pedro Henrique Gaspar de Figueiredo <ordep12@hotmail.com.br>
2022-09-27 22:49:34 -0300

    Linear Basics

--- Linear Gradient (Disney)/App.js
+++ Linear Gradient (Disney)/App.js
@@ -37,3 +37,56 @@ const styles = StyleSheet.create({
     height: 870,
   },
 });
+
+// Home.js
+
+// ...
+//       <Text>Home Screen</Text>
+//       <TouchableOpacity onPress={() => {}}>
+//         <LinearGradient
+//           start={{ x: 0, y: 0 }}
+//           end={{x: 1, y: 1 }}
+//           colors={['#5851DB', '#C13584', '#E1306C', '#FD1D1D', '#F77737']}
+//           style={styles.instagramButton}
+//           >
+//           <Text style={{ color: 'white' }}>
+//             Sign In With Instagram
+//           </Text>
+//         </LinearGradient>
+//       </TouchableOpacity>
+//       <LinearGradient
+//         start={{ x: 0, y: 0 }}
+//         end={{x: 1, y: 1 }}
+//         colors={[ 'red', 'yellow', 'green' ]}
+//         style={{
+//           alignItems: 'center',
+//           justifyContent: 'center',
+//           borderRadius: 10,
+//         }}
+//       >
+//         <TouchableOpacity
+//           onPress={() => {}}
+//           style={styles.signUpButton}
+//         >
+//           <Text>Sign Up</Text>
+//         </TouchableOpacity>
+//       </LinearGradient>
+
+// ...
+
+// // styles
+//   instagramButton: {
+//     paddingHorizontal: 40,
+//     paddingVertical: 10,
+//     borderRadius: 10,
+//     margin: 20
+//   },
+//   signUpButton: {
+//     margin: 1,
+//     width: 200,
+//     borderRadius: 10,
+//     paddingVertical: 10,
+//     alignItems: 'center',
+//     justifyContent: 'center',
+//     backgroundColor: 'white',
+//   },
