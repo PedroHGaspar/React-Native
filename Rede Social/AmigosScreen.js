84bb989163743ff43b7ac22df31a689facef5f9c
Pedro Henrique Gaspar de Figueiredo <ordep12@hotmail.com.br>
2022-10-25 10:03:21 -0300

    AmigosScreen e App.js

---
+++ Rede Social/AmigosScreen.js
@@ -0,0 +1,31 @@
+import {
+    Text,
+    TextInput,
+    View,
+    Image,
+    Pressable,
+    StyleSheet,
+    Vibration,
+  } from 'react-native';
+  //Para pegar as imagens
+  import * as ImagePicker from 'expo-image-picker';
+  //Hooks
+  import { useState, useContext } from 'react';
+  //Icone da imagem (Adicionar foto)
+  import { FontAwesome } from '@expo/vector-icons';
+  //Contexto
+  import { PostsContext } from './Contexto/PostsContext';
+  import { Ionicons } from '@expo/vector-icons'; 
+  
+  /*
+  Componente para adicionar novas memorias, ou seja, 
+  pega as informações e joga pro objeto memorias
+  */
+  export function Amigos() {
+  
+  }
+  const styles = StyleSheet.create({
+  
+  
+  });
+  
